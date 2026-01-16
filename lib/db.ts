d6303a5e3c6bfedf3b517047ef508cdf270c2389
Liam2705 <97888819+Liam2705@users.noreply.github.com>
2026-01-16 22:01:07 +0000

    Initial commit

---
+++ lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
+const prismaClient = () => {
+    return new PrismaClient({ adapter })
+}
+declare const globalThis: {
+    prismaGlobal: ReturnType<typeof prismaClient>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClient();
+
+export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
